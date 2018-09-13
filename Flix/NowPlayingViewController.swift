2307ca896d67759e97365a932f339d690d391a09
Serena-Wang <wang225y@mtholyoke.edu>
2018-09-12 20:01:57 -0400

    update

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -13,14 +13,29 @@ class NowPlayingViewController: UIViewController,UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
     override func viewDidLoad() {
+        
         tableView.rowHeight = 217
         super.viewDidLoad()
-        
+        self.activityIndicator.startAnimating()
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector (NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
+        fetchMovies()
+        self.activityIndicator.stopAnimating()
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
+        fetchMovies()
         
-        
+    }
+    
+    func fetchMovies(){
         let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -32,6 +47,8 @@ class NowPlayingViewController: UIViewController,UITableViewDataSource {
                 let movies = dataDictionary["results"] as! [[String:Any]]
                 self.movies = movies
                 self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+                
             }
         }
         task.resume()
