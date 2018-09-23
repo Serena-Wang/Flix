29cc8706d255754a17cdf38f6e7579fb8f6f94cf
Serena-Wang <wang225y@mtholyoke.edu>
2018-09-23 15:01:35 -0400

    update

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by yijin Wang on 9/23/18.
+//  Copyright © 2018 Yijin Wang. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
